4728134a848932795cded13103f84ec330e0c32b
leonardhw <leonardharyow@gmail.com>
2022-11-30 17:38:14 +0700

    Added screens, components, and navigators skeleton

---
+++ client/client-mobile/src/components/FormUser.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FormUser() {
+  return (
+    <View>
+      <Text>FormUser</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ client/client-mobile/src/components/ItemCardLarge.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ItemCardLarge() {
+  return (
+    <View>
+      <Text>ItemCardLarge</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
