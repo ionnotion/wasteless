f66ead7e59bfe6cf5f2b2a0b9817d9c331801973
leonardhw <leonardharyow@gmail.com>
2022-11-30 17:36:37 +0700

    Added screens, components, and navigators skeleton

---
+++ client/client-mobile/src/screens/SearchAllCategory.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SearchAllCategory() {
+  return (
+    <View>
+      <Text>SearchAllCategory</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ client/client-mobile/src/screens/SearchByCategory.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SearchByCategory() {
+  return (
+    <View>
+      <Text>SearchByCategory</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
